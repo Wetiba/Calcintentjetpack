58d19da432f62a792a0febf3a92c71f681c43bcd
Wetiba <erick@gmail.com>
2023-08-25 12:37:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calcintent/IntentActivity.kt
@@ -0,0 +1,162 @@
+package com.example.calcintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calcintent.ui.theme.CalcIntentTheme
+
+class IntentActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Intent()
+
+        }
+    }
+}
+
+@Composable
+fun Intent() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "CalcIntent App",
+            color = Color.Cyan,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 35.sp
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Camera",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Call",
+                fontSize = 30.sp)
+
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Sms",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Stk",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Share",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Email",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Dial",
+                fontSize = 30.sp)
+
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Intentpre() {
+Intent()
+}
